6ba1a8175935fc24318b9869acad5fc9051d6814
Jhonatan Castillo Salvador <jhonatancastillosalvador@hotmail.com>
2020-06-02 14:30:47 -0500

    commit 3
    commit 3

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,44 @@
 "use strict";
 
+var divRespuesta = document.getElementById("divRespuesta");
+var datoTextArea = document.getElementById("text-area-keyword-extraction");
+
 function obtenerJSON(dato = "") {
-  let frases = [dato];
-  fetch("/api", {
-    method: "POST",
-    body: JSON.stringify({ frases }),
-  })
-    .then((res) => {
-      res.json();
+  axios
+    .post("/api", {
+      frases: [dato],
     })
-    .catch((error) => {
+    .then(function (response) {
+      let btnCerrarDivRespuesta = document.createElement("button");
+      let spanCerrarDivRespuesta = document.createElement("span");
+      let h3Texto1 = document.createElement("h3");
+      btnCerrarDivRespuesta.setAttribute("type", "button");
+      btnCerrarDivRespuesta.setAttribute("class", "close");
+      btnCerrarDivRespuesta.setAttribute("data-dismiss", "alert");
+      btnCerrarDivRespuesta.setAttribute("aria-label", "Cerrar");
+      spanCerrarDivRespuesta.setAttribute("aria-hidden", "true");
+      spanCerrarDivRespuesta.innerHTML = "&times;";
+      h3Texto1.innerHTML = "Texto:";
+      divRespuesta.appendChild(btnCerrarDivRespuesta);
+      btnCerrarDivRespuesta.appendChild(spanCerrarDivRespuesta);
+      divRespuesta.appendChild(h3Texto1);
+
+      console.log(response.data.idiomas[0].primaryLanguage.name);
+      console.log(response.data.idiomas[0].primaryLanguage.confidenceScore);
+      //alert(response.data);
+      //console.log(response.data);
+    })
+    .catch(function (error) {
       console.error(error);
-    })
-    .then((response) => {
-      alert(response);
-      console.log("respuesta 2: ", response);
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  //divRespuesta.style.display = "none";
   $("#form-keyword-extraction")
     .parsley()
     .on("form:success", function (evento) {
       this.validationResult = false;
-      let datoTextArea = document.getElementById(
-        "text-area-keyword-extraction"
-      );
       obtenerJSON(datoTextArea.value);
     });
 });
