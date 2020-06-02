962f92be3cb8af622ca97a4f6f6b2fb63c6d1e49
Jhonatan Castillo Salvador <jhonatancastillosalvador@hotmail.com>
2020-06-02 08:24:43 -0500

    commit 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,31 @@
-$("#form-keyword-extraction")
-  .parsley()
-  .on("form:success", function (evento) {
-    evento.submitEvent.preventDefault();
-    console.log($("#text-area-keyword-extraction").val());
-  });
+"use strict";
+
+function obtenerJSON(dato = "") {
+  let frases = [dato];
+  fetch("/api", {
+    method: "POST",
+    body: JSON.stringify({ frases }),
+  })
+    .then((res) => {
+      res.json();
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .then((response) => {
+      alert(response);
+      console.log("respuesta 2: ", response);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  $("#form-keyword-extraction")
+    .parsley()
+    .on("form:success", function (evento) {
+      this.validationResult = false;
+      let datoTextArea = document.getElementById(
+        "text-area-keyword-extraction"
+      );
+      obtenerJSON(datoTextArea.value);
+    });
+});
